3aa3f2c7127dc441005d51d77d37a7b05ffdda90
Neer23mehta <mehtaneer143@gmail.com>
2025-02-25 17:46:13 +0530

    Add files via upload

---
+++ Postapi.jsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+//     const api = axios.create({
+//         baseURL:"https://jsonplaceholder.typicode.com"
+//     })
+
+// export const apiget = () => {
+//     return  api.get("/posts")
+// }
+
+// export const deletepost = (id) => {
+//     return api.delete(`/posts/${id}`);
+// }
+
+// export const postapi = (post) => {
+//     return api.post("/posts",post);
+// }
+
+// export const putapi = (id , post) => {
+//     return api.put(`/posts/${id}`,post);
+// }
+
+
+// const Api = axios.create({
+    // baseURL: `http://www.postalpincode.in/api/`
+
+
+// })
+
+// export const Apigets = (name) => {
+    // return Api.get(`/pincode/{city_name}`)
+// }
+
+// const Apis = axios.create({
+//     baseURL: "https://fakestoreapi.com/products"
+// })
+
+// export const Returnapi = () => {
+//     return Apis.get("")
+// }
+
+// const Apic = axios.create({
+//     baseURL:"https://v6.exchangerate-api.com/v6/1eeccfb59edc61c8c944107e"
+// })
+
+// export const getapic = (fromcurrency,tocurrency,inp) => {
+//     return Apic.get(`/pair/${fromcurrency}/${tocurrency}/${inp}`)
+// }
+
+
+// export const Apis = axios.create({
+//     baseURL:"https://fakestoreapi.com/products"
+// })
+
+// export const postapi = () => {
+    // return Api.post("http://localhost:3009/posts",Userid = userid,Cartitem = cartitem)
+// }
